16b413e920ea9723b24b409f420df540c24eb549
Tim <tpoot96@gmail.com>
2015-05-01 13:19:33 +0200

    Fix for slow internet

--- Code/pullPost.js
+++ Code/pullPost.js
@@ -1,31 +1,33 @@
 var url = "https://re.reddit.com/r/recipes/.json?jsonp=?";
-var posts = []; //gekke js arrays, spooky shit
+var json;
+var posts = [];
 var post1, post2, post3;
+
+
 
 function getPost(){
   var i, j;
   
-  $.getJSON(url, function(data, status){
-    i = 0;
-    j = 0;
-    while(i < 10){
-      if(data.data.children[j].data.link_flair_text == "Recipe"){
-        posts[i] = data.data.children[j].data.selftext;
-        i++;
-      }
-      j++;
-    } 			
-  });
+  i = 0;
+  j = 1;
+  while(i < 10 && j < 25){
+    if(json.data.children[j].data.link_flair_text === "Recipe"){
+      posts[i] = json.data.children[j].data.selftext;
+      i++;
+    }
+
+    j++;
+  }		
 }
 
 function setPosts(){
   var postnr1, postnr2, postnr3;
-
-  do{//niet meer dubbele posts
-        postnr1 = Math.floor((Math.random() * 10) + 1);
-        postnr2 = Math.floor((Math.random() * 10) + 1);
-        postnr3 = Math.floor((Math.random() * 10) + 1);
-  }while(postnr1 === postnr2 || postnr2 === postnr3 || postnr1 === postnr3)
+  do{
+    postnr1 = Math.floor((Math.random() * posts.length) + 1);
+    postnr2 = Math.floor((Math.random() * posts.length) + 1);
+    postnr3 = Math.floor((Math.random() * posts.length) + 1);
+    break;
+  }while(postnr1 === postnr2 || postnr2 === postnr3 || postnr1 === postnr3);
   
   post1 = posts[postnr1];
   post2 = posts[postnr2];
@@ -43,7 +45,10 @@ function buttonPress(){
 
 $(document).ready(function(){
   $("#RDG").click(function(){
-    buttonPress();
+    $.getJSON(url, function(data, status){
+      json = data;
+      buttonPress();  
+    });
   });
 });
 
